09989d7b4d9d2bceeab8b8fefde46e8e8dfd4889
ejc10d <112822833+ejc10d@users.noreply.github.com>
2023-03-02 17:37:15 -0500

    setting up original files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Profile from './components/Profile';
+
+function App() {
+    return (
+        <div className='profile-app'>
+            <Profile />
+        </div>
+    );
+}
+
+export default App;
