5405fe6138d7c55f7cf093920378a81ed62a38ce
ejc10d <112822833+ejc10d@users.noreply.github.com>
2023-03-04 07:11:36 -0500

    adjusting files for creating react app

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Profile from './components/Profile';
+
+function App() {
+  return (
+    <div className="profile-app">
+      <Header />
+      <Profile />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '';
+
+function Header() {
+    return (
+        <header className='header'>
+            <h1>Eliot's React Profile</h1>
+        </header>
+    )
+}
+
+export default Header;
