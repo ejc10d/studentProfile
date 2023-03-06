6122a6f008dcade9696ab557feef38aec088d659
ejc10d <112822833+ejc10d@users.noreply.github.com>
2023-03-05 19:35:45 -0500

    added about page info

---
+++ my-app/src/components/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function About () {
+    return (
+        <div>
+            <p className="content">Title</p>
+            <hr />
+            <img className="profilePhoto" src={process.env.PUBLIC_URL + '/img/profilePhoto.jpg'} alt="profile photo"/>
+            <p className="content">
+            This is the place where my "about me" will go. 
+            </p>
+        </div>
+    );
+}
+
+export default About;
