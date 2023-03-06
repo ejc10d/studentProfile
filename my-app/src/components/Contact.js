9592c4919b61280d4c1cdef388693d2c0cc3b252
ejc10d <112822833+ejc10d@users.noreply.github.com>
2023-03-05 20:38:28 -0500

    moved logic from contact into footer component file

--- my-app/src/components/Contact.js
+++ my-app/src/components/Contact.js
@@ -1,23 +1,7 @@
 import React from 'react';
 
-function Footer() {
-    return (
-        <footer className='footer'>
-            <div>
-                <p>
-                    <a href='https://github.com/ejc10d'>
-                        GitHub Profile
-                    </a>
-                    <a href='http://linkedin.com/eliotcrandall'>
-                        LinkedIn Profile
-                    </a>
-                    <a href='http://twitter.com/ejc10d'>
-                        Twitter Profile
-                    </a>
-                </p>
-            </div>
-        </footer>
-    );
+function Contact() {
+
 }
 
-export default Footer;
+export default Contact;
