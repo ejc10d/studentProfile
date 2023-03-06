b0e2bc8a887a91ee3e4f4ab1a52581ce40b0337f
ejc10d <112822833+ejc10d@users.noreply.github.com>
2023-03-05 20:36:20 -0500

    added footer

---
+++ my-app/src/components/Contact.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer className='footer'>
+            <div>
+                <p>
+                    <a href='https://github.com/ejc10d'>
+                        GitHub Profile
+                    </a>
+                    <a href='http://linkedin.com/eliotcrandall'>
+                        LinkedIn Profile
+                    </a>
+                    <a href='http://twitter.com/ejc10d'>
+                        Twitter Profile
+                    </a>
+                </p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
