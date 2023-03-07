2b140dd41497731c745326cd7b8f8fbf4aa3c386
ejc10d <112822833+ejc10d@users.noreply.github.com>
2023-03-06 23:54:18 -0500

    attempting new route for header

--- my-app/src/components/Header.js
+++ my-app/src/components/Header.js
@@ -4,9 +4,14 @@ import { Link } from 'react-router-dom';
 function Navigation() {
     return (
         <nav>
-            
+            <ul>
+                <li><Link to='/about'>About</Link></li>
+                <li><Link to='/portfolio'>Portfolio</Link></li>
+                <li><Link to='/contact'>Contact</Link></li>
+                <li><Link to='/resume'>Resume</Link></li>
+            </ul>
         </nav>
-    )
+    );
 }
 
 
