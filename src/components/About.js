ea7c15b7695379b61710ed942078221ccef06f74
ejc10d <112822833+ejc10d@users.noreply.github.com>
2023-03-31 22:33:17 -0400

    removed twitter link and edited bio

--- src/components/About.js
+++ src/components/About.js
@@ -8,11 +8,12 @@ function About () {
             <img className="profile" alt="profile" src={require('../assets/images/headshot.png')} />
             <p className="content m-2 max-w-sm text-center">
             Hello World! < br />
-            My name is Eliot and I'm an aspiring web developer. <br />
+            My name is Eliot and I'm a junior web developer. <br />
             </p>
             <p className='content m-2 max-w-md'>
-            I have been intrigued by code for quite some time now. I always liked manipulating code and getting to see my results show up on a page. I learned some basic HTML to write some emails for a non-profit I worked with when I was younger. 
-            <br/> <br/>Currently, I'm finishing a coding bootcamp through UCF and hope to pursue a career in this exciting new career. The part that I've come to love about coding is that there is always something more to learn and some new technology to dive into and attempt to understand. 
+            I recetly completed a full-stack coding bootcamp through UCF and hope to pursue a career in this exciting new career. The part that I've come to love about coding is that there is always something more to learn and some new technology to dive into and attempt to understand. 
+            <br/> <br/>
+            I have been intrigued by code for quite some time. I always liked manipulating the code of my MySpace page and getting to see my results show up on the front-end. I discovered HTML's usefulness when I learned some HTML to write some emails for a non-profit when I was younger. And now, I've found an appreciation for the back-end that give web applications purpose.
             </p>
         </div>
     );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,16 +6,25 @@ function Footer() {
             <hr />
             <div className='m-5'>
                 <div  className='display: flex space-x-10 justify-center'>
-                    <a href='https://github.com/ejc10d' className='navbutton'>
+                    <a 
+                    href='https://github.com/ejc10d' 
+                    target="_blank"
+                    rel="noreferrer"
+                    className='navbutton'>
                         GitHub Profile
                     </a> 
                     <section>|</section>
-                    <a href='https://www.linkedin.com/in/eliot-crandall-3b8a3669/' className='navbutton'>
+                    <a 
+                    href='https://www.linkedin.com/in/eliot-crandall-3b8a3669/' 
+                    target="_blank"
+                    rel="noreferrer"
+                    className='navbutton'>
                          LinkedIn Profile
-                    </a> <section>|</section>
+                    </a> 
+                    {/* <section>|</section>
                     <a href='http://twitter.com/eliot_jay' className='navbutton'>
                         Twitter Profile
-                    </a>
+                    </a> */}
                 </div>
                 <div className='text-center m-5 text-xs'>Created by Eliot Crandall</div>
             </div>
